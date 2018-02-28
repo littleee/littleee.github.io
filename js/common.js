d0a35fc4aa12791c90c9df95a3b6f320d3e821dd
liubochao <liubochao@oneapm.com>
2018-02-28 16:00:12 +0800

    Site updated: 2018-02-28 16:00:11

--- js/common.js
+++ js/common.js
@@ -3,9 +3,21 @@ $(function(){
     if($(window).scrollTop() > 60) {
       $('.color-bar').addClass('out')
       $('.header').addClass('out')
+      $('.nav-collapse').css('top','53px')
     } else {
       $('.color-bar').removeClass('out')
       $('.header').removeClass('out')
+      $('.nav-collapse').css('top','59px')
     }
   })
+
+    $(document).click(function(){
+       if($('.collapse').hasClass('in')){
+$('#nav-btn').trigger('click');
+       }
+    })
+    $('.nav-collapse').click(function(e){
+       e.stopPropagation();
+    })
+
 })
