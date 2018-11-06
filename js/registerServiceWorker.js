adfca8dcaf7722c86e29756fa32907d826ab4055
littleee <liubochao_job@126.com>
2018-11-06 17:48:40 +0800

    Site updated: 2018-11-06 17:48:40

---
+++ js/registerServiceWorker.js
@@ -0,0 +1,26 @@
+
+$(function(){
+  const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
+      // [::1] is the IPv6 localhost address.
+      window.location.hostname === '[::1]' ||
+      // 127.0.0.1/8 is considered localhost for IPv4.
+      window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+      )
+  );
+
+  if ('serviceWorker' in navigator) {
+    console.log(1111);
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }).catch(function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+
+})
